79ca898b690f9c959a10abb411bda409f8071818
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-10 23:35:07 +0600

    update

---
+++ 146/test.cpp
@@ -0,0 +1,28 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+int main()
+{
+    Boost;
+    string s = "1";
+    int t = stoi(s);
+    cout << t << endl;
+
+    return 0;
+}
