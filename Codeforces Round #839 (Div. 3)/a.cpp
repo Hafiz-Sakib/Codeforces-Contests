fbd9c1fa0f4dd36260bf0b0bdb0ce4046af0a572
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2022-12-18 21:08:44 +0600

    update

---
+++ Codeforces Round #839 (Div. 3)/a.cpp
@@ -0,0 +1,43 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long int
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+void Boom()
+{
+    int a, b;
+    char c;
+    cin >> a;
+    cin >> c;
+    cin >> b;
+    cout << a + b << endl;
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
