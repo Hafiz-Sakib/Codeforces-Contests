10f77dc268819153dec3800beda87e9785f6a415
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2022-12-24 21:12:28 +0600

    update

---
+++ Biweekly Contest 94 LEETCODE/a.cpp
@@ -0,0 +1,70 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long int
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+void Boom()
+{
+    ll f;
+    cin >> f;
+    vector<ll> forts(f);
+    for (ll i = 0; i < f; i++)
+    {
+        cin >> forts[i];
+    }
+    vector<int> ans;
+
+    int n = forts.size();
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (forts[i] == 1 || forts[i] == -1)
+            ans.push_back(i);
+    }
+
+    int res = 0;
+
+    if (ans.size() > 0)
+    {
+
+        for (int i = 0; i < ans.size() - 1; i++)
+        {
+
+            if ((forts[ans[i]] * forts[ans[i + 1]]) == -1)
+            {
+                res = max(res, abs(ans[i + 1] - ans[i]) - 1);
+            }
+        }
+    }
+    cout << res << endl;
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
