620da112890323450c84b7fb4082de95fc62f4a2
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-01-04 19:13:24 +0600

    Problem Solved

--- Codeforces Beta Round 64/test.c
+++ Codeforces Beta Round 64/test.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <conio.h>
+
 int main()
 {
-    char a[] = "saKiB";
-    char b[10] = toupper(a);
-    printf("%s", b);
+    int n, count = 0;
+    scanf("%d", &n);
+    for (int i = n; i > 1; i++)
+    {
+        if (n % 2 == 0)
+        {
+            n = (n / 2);
+            count++;
+        }
+        else
+        {
+            n = (3 * n) + 1;
+            count++;
+        }
+    }
+    printf("%d", count);
 
     return 0;
 }
