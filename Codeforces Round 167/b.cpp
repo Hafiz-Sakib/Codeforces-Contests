57956648e61cc6d93547beb8117b7ab7bb384248
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-01-19 02:06:28 +0600

    Problem Solved

---
+++ Codeforces Round 167/b.cpp
@@ -0,0 +1,11 @@
+/// Bismillahir Rahmanir Rahim
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll n;
+    cin >> n;
+    ((ceil(log2(n)) == floor(log2(n)))) ? cout << "YES" : cout << "NO";
+    return 0;
+}
