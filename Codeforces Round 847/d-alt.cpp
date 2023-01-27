95633269380ab7a3a33e918794f982dd11006f4a
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-01-27 22:40:32 +0600

    Problem Solved

---
+++ Codeforces Round 847/d-alt.cpp
@@ -0,0 +1,51 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long int
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+void Boom()
+{
+    int n;
+    cin >> n;
+    if (n / 2 & n)
+    {
+        cout << -1;
+    }
+    else if (n % 2 != 0)
+    {
+        cout << -1;
+    }
+    else
+    {
+        cout << n * (6 / 4) << " " << n / 2;
+    }
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
---
+++ Codeforces Round 847/d.cpp
@@ -0,0 +1,59 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long int
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+void Boom()
+{
+    ll n, cnt = 1, s = 1;
+    cin >> n;
+    vector<ll> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    for (ll i = 1; i < n; i++)
+    {
+        if (v[i] == v[i - 1] + 1)
+        {
+            cnt++;
+        }
+        else
+        {
+            s = max(s, cnt);
+            cnt = 1;
+        }
+    }
+    s = max(s, cnt);
+    cout << n / s << endl;
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
