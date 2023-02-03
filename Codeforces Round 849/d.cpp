ec4f03e891def798cba6373a74e9cc36bfa28d65
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-02-03 21:46:39 +0600

    Problem Solved

--- Codeforces Round 849/d.cpp
+++ Codeforces Round 849/d.cpp
@@ -1,6 +1,6 @@
 /*
 
-string author;
+ sing author;
 author = Hafiz_Sakib;
 
 */
@@ -9,17 +9,58 @@ author = Hafiz_Sakib;
 using namespace std;
 #define endl "\n"
 #define ll long long int
-#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define vi vector<int>
+#define vvi vector<vi>
+#define ignore cin.ignore(numeric_limits<seamsize>::max(), '\n')
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
-
 #define debug(x) cerr << x << endl;
 #define here fprintf(stderr, "====I am Here====\n");
-
 void Boom()
 {
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vvi cnt(n, vi(26, 0));
+    for (int i = 0; i < n; i++)
+
+    {
+        if (i)
+        {
+            cnt[i] = cnt[i - 1];
+        }
+        cnt[i][s[i] - 'a']++;
+    }
+
+    vi sum = cnt.back();
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++)
+
+    {
+        vi tmp = sum;
+        for (int j = 0; j < 26; j++)
+        {
+            tmp[j] -= cnt[i][j];
+        }
+        int crnt = 0;
+        for (int j = 0; j < 26; j++)
+
+        {
+            if (cnt[i][j])
+            {
+                crnt++;
+            }
+            if (tmp[j])
+            {
+                crnt++;
+            }
+        }
+        ans = max(ans, crnt);
+    }
+    cout << ans << endl;
 }
 
 int main()
