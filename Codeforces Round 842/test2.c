b0da486f148d44d0becf62869b454aed1a59c267
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-02-05 21:32:10 +0600

    Problem Solved

---
+++ Codeforces Round 842/test2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int a[100][100], csum, rsum, i, j, row, column, k = 0, idx = 0;
+    printf("Enter the row and the column\n");
+    scanf("%d %d", &row, &column);
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (i = 0; i < row; i++)
+    {
+        rsum = 0;
+        for (j = 0; j < column; j++)
+        {
+            rsum += a[i][j];
+        }
+        printf("Sum of %d Row is = %d\n", (i + 1), rsum);
+    }
+
+    printf("\n");
+
+    for (i = 0; i < column; i++)
+    {
+        csum = 0;
+        for (j = 0; j < row; j++)
+        {
+            csum += a[j][i];
+        }
+        printf("Sum of %d Column is = %d\n", (i + 1), csum);
+    }
+
+    printf("\n");
+
+    return 0;
+}
