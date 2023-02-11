0db5eabaf7106f4ecb395780c793c42c8b6edb81
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-02-11 16:59:29 +0600

    Problem Solved

---
+++ Code/j.cpp
@@ -0,0 +1,45 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long int
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define YES cout << "Yes\n"
+#define NO cout << "No\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+int main()
+{
+    Boost;
+    int t = 1, a, b, ans;
+    cin >> t;
+    for (ll i = 1; i <= t; i++)
+    {
+        ll n, k, sum = 0;
+        cin >> n >> k;
+        vector<ll> v(n);
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        a = (v[n - 1] * n);
+        b = v[0];
+        ans = a - b;
+        cout << "Case " << i << ": " << ans << endl;
+    }
+    return 0;
+}
