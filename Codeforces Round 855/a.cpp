1fcca52e3d03fdfde79698d6c33dc35f465aea23
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-03-03 11:49:24 +0600

    Problem Solved

--- Codeforces Round 855/a.cpp
+++ Codeforces Round 855/a.cpp
@@ -1,3 +1,4 @@
+// Bismillahir Rahmanir Rahim
 /*
 
 string author;
@@ -7,30 +8,27 @@ author = Hafiz_Sakib;
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long int
+typedef long long int ll;
+typedef unsigned long long ull;
+
 typedef vector<ll> vi;
+typedef vector<double> vd;
 typedef vector<string> vs;
+typedef vector<char> vc;
 typedef vector<vector<ll>> vvi;
 typedef vector<pair<ll, ll>> vpii;
 typedef vector<pair<string, ll>> vpsi;
 typedef pair<ll, ll> pii;
-typedef pair<string, ll> psi;
-typedef pair<ll, string> pis;
-typedef pair<double, double> pdd;
 typedef map<ll, ll> mii;
-typedef map<ll, bool> mib;
 typedef map<string, ll> msi;
-typedef map<ll, string> mis;
 typedef set<ll> si;
 typedef set<string> ss;
-typedef set<double> sd;
 
 #define pb push_back
 #define mp make_pair
-#define pob pop_back
 #define in insert
-#define F first
-#define S second
+#define fi first
+#define se second
 #define space ' '
 #define endl "\n"
 
@@ -38,91 +36,32 @@ typedef set<double> sd;
 #define e() end()
 #define all(data) data.begin(), data.end()
 #define rall(data) data.rbegin(), data.rend()
-#define sz(x) (int)x.size()
 #define vecMax(data) *max_element(data.begin(), data.end())
 #define vecMin(data) *min_element(data.begin(), data.end())
 #define vecSum(data) accumulate(data.begin(), data.end(), 0)
-#define vecCount(data, key) count(data.begin(), data.end(), key)
-#define Reverse(data) reverse(data.begin(), data.end())
-#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
-#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
 
 #define yes cout << "YES\n"
 #define no cout << "NO\n"
-#define YES cout << "Yes\n"
-#define NO cout << "No\n"
+
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
 #define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-#define gcd(a, b) __gcd(a, b)
-#define sqr(a) ((a) * (a))
-#define MOD 1000000007
-#define debug(x) cerr << x << endl;
-#define here fprintf(stderr, "====I am Here====\n");
-
-void SieveOfEratosthenes(ll n)
-{
-    bool prime[n + 1];
-    memset(prime, true, sizeof(prime));
-    for (ll p = 2; p * p <= n; p++)
-    {
-        if (prime[p] == true)
-        {
-            {
-                for (ll i = p * p; i <= n; i += p)
-                {
-                    prime[i] = false;
-                }
-            }
-        }
-    }
-
-    for (ll p = 2; p <= n; p++)
-    {
-        if (prime[p])
-        {
-            cout << p << " ";
-        }
-    }
-}
-
 void Boom()
 {
-
-    // ll n;
-    // string s;
-    // cin >> n >> s;
-    // stringUpper(s);
-    // if (s[0] != 'M')
-    // {
-    //     no;
-    //     return;
-    // }
-    // set<char> st;
-    // for (auto u : s)
-    // {
-    //     st.in(u);
-    // }
-    // if (st.size() < 4)
-    // {
-    //     no;
-    //     return;
-    // }
-    // for (auto u : st)
-    // {
-    //     cout << u << endl;
-    // }
-    SieveOfEratosthenes(1000000);
 }
 
 int main()
 {
     Boost;
+
     int t = 1;
-    // cin >> t;
+    cin >> t;
     while (t--)
     {
         Boom();
