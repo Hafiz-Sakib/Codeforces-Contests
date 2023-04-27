75ec216fa472a75f14c15519bd32b02c55b3eda0
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-04-27 08:49:38 +0600

    New Contetst

---
+++ Codeforces Round 867/a.cpp
@@ -0,0 +1,96 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+    ll n, time;
+    cin >> n >> time;
+    vi a(n), b(n);
+    for (auto &nn : a)
+    {
+        cin >> nn;
+    }
+    for (auto &nn : b)
+    {
+        cin >> nn;
+    }
+    ll ans = -1;
+    for (ll i = 0; i < n; i++)
+    {
+        if (a[i] <= time - i)
+        {
+            if (ans == -1 || b[i] > b[ans - 1])
+            {
+                ans = i + 1;
+            }
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
