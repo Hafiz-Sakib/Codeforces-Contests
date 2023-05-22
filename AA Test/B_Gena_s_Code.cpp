7af170f4142af36b6854dc5e0d6696ac82533d84
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-22 16:44:22 +0600

    TEST

---
+++ AA Test/B_Gena_s_Code.cpp
@@ -0,0 +1,123 @@
+// https://codeforces.com/contest/614/problem/B
+
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+bool check(string x)
+{
+    int ones = 0, num = 0;
+    for (int i = 0; i < x.size(); i++)
+        if (x[i] == '1')
+            ones++;
+        else if (x[i] != '0')
+            num++;
+    if (ones > 1 || num > 0)
+        return false;
+
+    return true;
+}
+void Boom()
+{
+    string s, x, q;
+    bool found = false;
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> x;
+        if (x == "0")
+        {
+            found = true;
+        }
+        if (check(x))
+        {
+            x.erase(0, 1);
+            if (!x.empty())
+            {
+                s += x;
+            }
+        }
+        else
+        {
+            q = x;
+        }
+    }
+    if (q.empty())
+    {
+        q = "1";
+    }
+    if (found)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << q + s << endl;
+    }
+}
+
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
