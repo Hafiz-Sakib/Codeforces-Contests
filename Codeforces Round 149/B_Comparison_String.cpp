24b6fae7c1123ba7ec8da9d852ac3d8b48be5c9b
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-25 21:35:15 +0600

    Submit the Code

---
+++ Codeforces Round 149/B_Comparison_String.cpp
@@ -0,0 +1,97 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef set<string> ss;
+
+#define pb push_back
+#define m make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+    ll n, maxi = 1, eq = 1;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (s[i] == s[i + 1])
+        {
+            eq++;
+        }
+        else
+        {
+            maxi = max(maxi, eq);
+            eq = 1;
+        }
+    }
+    maxi = max(maxi, eq);
+    cout << maxi + 1 << endl;
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
