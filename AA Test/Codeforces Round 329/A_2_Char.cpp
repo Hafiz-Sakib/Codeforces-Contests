c2c3e6ac2af1aaeec8f797ed5678267464ace4f6
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-06-02 21:26:36 +0600

    Submit

---
+++ AA Test/Codeforces Round 329/A_2_Char.cpp
@@ -0,0 +1,110 @@
+// https://codeforces.com/contest/593/problem/A
+
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+const int mx = 1e8 + 123;
+#define mod 1000000007
+const double eps = 1e-12;
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define sz(v) (int)v.size()
+int se[110][300], S[110];
+void Boom()
+{
+    int i, j, k, n, m;
+    cin >> n;
+    string s;
+    for (i = 0; i < n; i++)
+    {
+        cin >> s;
+        S[i] = sz(s);
+        for (auto x : s)
+            se[i][x] = 1;
+    }
+    int ans = 0;
+    for (char ch = 'a'; ch <= 'z'; ch++)
+    {
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            int sum = 0;
+            for (i = 0; i < n; i++)
+            {
+                bool ok = 1;
+                for (char p = 'a'; p <= 'z'; p++)
+                {
+                    if (p != ch && p != c && se[i][p])
+                    {
+                        ok = 0;
+                        break;
+                    }
+                }
+                if (ok)
+                    sum += S[i];
+            }
+            ans = max(ans, sum);
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
