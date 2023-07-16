538b770d8ed0a437ffd7ac4d1d8840316d163051
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-07-16 10:17:17 +0600

    Problem Solved

---
+++ AA Test/Eductional Codeforces Round 32/D_Almost_Identity_Permutations.cpp
@@ -0,0 +1,95 @@
+// https://codeforces.com/contest/888/problem/D
+
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define mod 1000000007
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+const int lg = 20;
+ll calc(ll n, ll k)
+{
+    ll res = 1;
+    for (int i = 0; i < k; i++)
+    {
+        res *= (n - i);
+    }
+    for (int i = 1; i <= k; i++)
+    {
+        res /= i;
+    }
+    return res;
+}
+ll n, m, ans, parish[5];
+int main()
+{
+    Boost;
+    cin >> n >> m;
+    parish[0] = 1;
+    parish[1] = 0;
+    parish[2] = 1;
+    parish[3] = 2;
+    parish[4] = 9;
+    for (int i = 0; i <= m; i++)
+    {
+        ans += parish[i] * calc(n, i);
+    }
+    cout << ans;
+}
