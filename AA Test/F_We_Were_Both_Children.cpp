2b6590147f59e3b952b2ca63305ee26a3536d341
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-07-23 10:31:19 +0600

    F Solved

---
+++ AA Test/F_We_Were_Both_Children.cpp
@@ -0,0 +1,104 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMaj(data) *maj_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    vi freq(n + 2, 0);
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > n)
+        {
+            continue;
+        }
+        freq[a[i]]++;
+    }
+
+    ll ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        ll here = 0;
+        for (ll j = 1; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                here += freq[j];
+                if (j * j != i)
+                {
+                    here += freq[i / j];
+                }
+            }
+        }
+        ans = max(ans, here);
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
