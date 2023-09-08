069896aa44f31d38a92fe22a71fb68b157a90f54
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-09-08 23:03:31 +0600

    Code Updates

--- AA Test/A_Jeff_and_Digits.cpp
+++ AA Test/A_Jeff_and_Digits.cpp
@@ -63,6 +63,40 @@ const double eps = 1e-12;
 
 void Boom()
 {
+    int n;
+    ll cnt0, cnt5, actual;
+    cin >> n;
+    actual = cnt5 = cnt0 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        if (a == 5)
+            cnt5++;
+        else
+            cnt0++;
+        if ((cnt5 * 5) % 9 == 0)
+            actual = cnt5;
+    }
+    if (actual != 0 && cnt0 > 0)
+    {
+        for (int i = 0; i < actual; i++)
+        {
+            cout << 5;
+        }
+        for (int i = 0; i < cnt0; i++)
+        {
+            cout << 0;
+        }
+    }
+    else if (cnt0 > 0)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << -1;
+    }
 }
 
 int main()
@@ -70,7 +104,6 @@ int main()
     Boost;
 
     int t = 1;
-    cin >> t;
     while (t--)
     {
         Boom();
