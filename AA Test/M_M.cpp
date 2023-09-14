1c66fb1d34434e92adad5a940a647cdab99b50e4
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-09-14 12:34:01 +0600

    Update

---
+++ AA Test/M_M.cpp
@@ -0,0 +1,139 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define mod 1000000007
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+bool is_ok(int n, vector<string> &grid)
+{
+    for (const string &row : grid)
+    {
+        int cntB = 0, cntW = 0;
+        for (char c : row)
+        {
+            if (c == 'B')
+            {
+                cntB++;
+            }
+            else
+            {
+                cntW++;
+            }
+        }
+        if (cntB != cntW)
+        {
+            return false;
+        }
+    }
+
+    for (int col = 0; col < n; col++)
+    {
+        int cntB = 0, cntW = 0;
+        for (int row = 0; row < n; row++)
+        {
+            if (grid[row][col] == 'B')
+            {
+                cntB++;
+            }
+            else
+            {
+                cntW++;
+            }
+        }
+        if (cntB != cntW)
+        {
+            return false;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - 2; j++)
+        {
+            if ((grid[i][j] == grid[i][j + 1] && grid[i][j + 1] == grid[i][j + 2]) ||
+                (grid[j][i] == grid[j + 1][i] && grid[j + 1][i] == grid[j + 2][i]))
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void Boom()
+{
+    int n;
+    cin >> n;
+    vs grid(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> grid[i];
+    }
+    bool ans = is_ok(n, grid);
+    cout << ans << endl;
+}
+
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
