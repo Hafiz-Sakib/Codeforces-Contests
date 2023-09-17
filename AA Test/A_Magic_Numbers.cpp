091efb3bdc0ff5525b43bc66f36af3ca5369c65e
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-09-17 23:38:20 +0600

    COde Updates

---
+++ AA Test/A_Magic_Numbers.cpp
@@ -0,0 +1,97 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define mod 1000000007
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+    string s, a = "1", b = "14", c = "144";
+    int i, j, k;
+    cin >> s;
+    for (i = 0; i < s.size(); i++)
+    {
+        if (s.substr(i, 3) == c)
+        {
+            i += 2;
+        }
+        else if (s.substr(i, 2) == b)
+        {
+            i++;
+        }
+        else if (s[i] == '1')
+        {
+            continue;
+        }
+        else
+        {
+            no;
+            return;
+        }
+    }
+    yes;
+}
+
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
