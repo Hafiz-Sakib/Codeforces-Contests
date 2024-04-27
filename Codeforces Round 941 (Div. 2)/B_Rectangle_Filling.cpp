635f434e6c056ece01ca901ac52f9555e22c04cd
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-04-27 23:00:40 +0600

    Solved B of Div 2 941

--- Codeforces Round 941 (Div. 2)/B_Rectangle_Filling.cpp
+++ Codeforces Round 941 (Div. 2)/B_Rectangle_Filling.cpp
@@ -1,72 +1,163 @@
-#include <iostream>
-#include <vector>
+// Bismillahir Rahmanir Rahim
+/*
 
-using namespace std; // Add this line to import the std namespace
+string author;
+author = Hafiz_Sakib;
 
-class Solution
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef pair<string, ll> psi;
+typedef pair<ll, string> pis;
+typedef pair<double, double> pdd;
+typedef map<ll, ll> mii;
+typedef map<ll, bool> mib;
+typedef map<string, ll> msi;
+typedef map<ll, string> mis;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef set<string> ss;
+typedef set<double> sd;
+typedef queue<ll> qu;
+typedef queue<pii> qupii;
+typedef priority_queue<ll> pq;
+typedef priority_queue<pii> pqpii;
+typedef priority_queue<string> pqs;
+typedef priority_queue<ll, vi, greater<ll>> pqg;
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+#define pb push_back
+#define mp make_pair
+#define pob pop_back
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define lb lower_bound
+#define ub upper_bound
+
+#define left(node) (node * 2)
+#define mid(l, r) ((r + l) >> 1)
+#define right(node) (node * 2 + 1)
+
+#define b() begin()
+#define e() end()
+#define sz(x) (int)x.size()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define Reverse(data) reverse(data.begin(), data.end())
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+#define vecCount(data, key) count(data.begin(), data.end(), key)
+
+#define setp(n) fixed << setprecision(n)
+#define fr(a, b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define mem(arr, val) memset(arr, val, sizeof(arr));
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define FOR(data) for (auto it = data.begin(); it != data.end(); it++)
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+#define sqr(a) ((a) * (a))
+#define mod 1000000007
+#define mx_int_prime 999999937
+
+const double PI = acos(-1);
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
 {
-public:
-    bool canMakeSquare(vector<vector<char>> &grid)
+    int n, m;
+    cin >> n >> m;
+    vector<string> gr;
+
+    for (int i = 0; i < n; i++)
     {
-        for (int i = 0; i < grid.size(); i++)
+        string row;
+        cin >> row;
+        gr.push_back(row);
+    }
+
+    string ans = "YES";
+
+    if (gr[0][0] != gr[n - 1][m - 1])
+    {
+        bool impossible = true;
+        for (int j = 0; j < m - 1; j++)
         {
-            for (int j = 0; j < grid[i].size(); j++)
+            if (gr[0][j] != gr[0][j + 1] || gr[n - 1][j] != gr[n - 1][j + 1])
             {
-                int b = 0;
-                int w = 0;
-                for (int k = i; k < i + 2 && k < grid.size(); k++)
-                {
-                    for (int l = j; l < j + 2 && l < grid[0].size(); l++)
-                    {
-                        if (grid[k][l] == 'B')
-                        {
-                            b++;
-                        }
-                        else
-                        {
-                            w++;
-                        }
-                    }
-                }
-                if (b >= 3 || w >= 3)
-                {
-                    return true;
-                }
+                impossible = false;
+                break;
             }
         }
-        return false;
+
+        if (impossible)
+        {
+            ans = "NO";
+        }
+
+        impossible = true;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (gr[i][0] != gr[i + 1][0] || gr[i][m - 1] != gr[i + 1][m - 1])
+            {
+                impossible = false;
+                break;
+            }
+        }
+
+        if (impossible)
+        {
+            ans = "NO";
+        }
     }
-};
+
+    cout << ans << endl;
+}
 
 int main()
 {
-    int t;
-    cin >> t; // Input number of test cases
+    Boost;
 
+    int t = 1;
+    cin >> t;
     while (t--)
     {
-        int n, m;
-        cin >> n >> m; // Input grid dimensions
-
-        vector<vector<char>> grid(n, vector<char>(m));
-        for (int i = 0; i < n; ++i)
-        {
-            for (int j = 0; j < m; ++j)
-            {
-                cin >> grid[i][j]; // Input grid values
-            }
-        }
-
-        Solution solution;
-        bool result = solution.canMakeSquare(grid);
-        if (result)
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "NO" << endl;
-        }
+        // cout<<"Case "<<t<<" : ";
+        Boom();
     }
 
     return 0;
