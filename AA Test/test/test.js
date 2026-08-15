e750cf0ff4387324726e96f4027aac5a05746e2c
Hafiz Sakib <hafizsakib5@gmail.com>
2026-08-15 11:03:00 +0600

    Add profile information and console output in test.js

--- AA Test/test/test.js
+++ AA Test/test/test.js
@@ -1,1 +1,36 @@
-console.log("helloooo");
+const profile = {
+  name: "Mohammad Hafizur Rahman Sakib",
+  title: "Full-Stack Developer",
+  skills: [
+    "React",
+    "Next.js",
+    "Node.js",
+    "MongoDB",
+    "Docker",
+    "AWS",
+    "TypeScript",
+    "GraphQL",
+  ],
+  competitive: {
+    codeforces: "Pupil | 1200+",
+    leetcode: "1532 Rating",
+    solved: "550+ Problems",
+  },
+  hardWorker: true,
+  quickLearner: true,
+  hireable: () => true,
+};
+
+console.log(`
+Name: ${profile.name}
+Title: ${profile.title}
+Skills: ${profile.skills.join(", ")}
+
+Codeforces: ${profile.competitive.codeforces}
+LeetCode: ${profile.competitive.leetcode}
+Problems Solved: ${profile.competitive.solved}
+
+Hard Worker: ${profile.hardWorker}
+Quick Learner: ${profile.quickLearner}
+Hireable: ${profile.hireable()}
+`);
